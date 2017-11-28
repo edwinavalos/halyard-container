31db9eda54d526b8696240fe5ddf74027096a754
Edwin Avalos <edwin@edwins-mbp.lan>
2017-11-27 19:03:13 -0600

    Do a docker login for the push
    Quiet the gradle output

--- prep_and_build.sh
+++ prep_and_build.sh
@@ -3,13 +3,13 @@
 VERSION=version-0.37.0
 TAG=$(grep -Eo "[0-9]*\.[0-9]*\.[0-9]*" <<< "${VERSION}")
 [ -e halyard ] && rm -rf halyard
-[ -e .gradle ] && rm -rf .gradle 
+[ -e .gradle ] && rm -rf .gradle
 git clone https://github.com/spinnaker/halyard.git
 cd halyard
 git checkout tags/"${VERSION}"
 export GRADLE_USER_HOME=../.gradle
-./gradlew halyard-web:installDist -x test
-./gradlew halyard-cli:installDist -x test
+./gradlew halyard-web:installDist -x test -q
+./gradlew halyard-cli:installDist -x test -q
 cd ..
 
 docker build -f Dockerfile -t edwinavalos/halyard:"${TAG}" .
