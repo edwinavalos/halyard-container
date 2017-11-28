0a9a32281d69d2a4fcd1515a21f75470c670171e
Edwin Avalos <edwin@edwins-mbp.lan>
2017-11-27 18:49:55 -0600

    Adding some automated builds

--- prep_and_build.sh
+++ prep_and_build.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash -e
+#!/bin/bash -e
 
 VERSION=version-0.37.0
 TAG=$(grep -Eo "[0-9]*\.[0-9]*\.[0-9]*" <<< "${VERSION}")
 [ -e halyard ] && rm -rf halyard
+[ -e .gradle ] && rm -rf .gradle 
 git clone https://github.com/spinnaker/halyard.git
 cd halyard
 git checkout tags/"${VERSION}"
